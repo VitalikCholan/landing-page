155da9bbe14b60199a5158a1fd9be0b911067ea0
Віталій Чолан <vitalikcolan@gmail.com>
2024-05-17 18:15:22 +0300

    added smooth transition for hamburger menu

--- js/hamburgerMenu.js
+++ js/hamburgerMenu.js
@@ -2,5 +2,11 @@ const toggleButton = document.querySelector('.toggle-button');
 const dropdown = document.querySelector('.dropdown');
 
 toggleButton.addEventListener('click', () => {
-  dropdown.classList.toggle('active');
+  const isActive = dropdown.classList.toggle('active');
+
+  if (isActive) {
+    dropdown.style.maxHeight = `${dropdown.scrollHeight}px`;
+  } else {
+    dropdown.style.maxHeight = '0px';
+  }
 });
