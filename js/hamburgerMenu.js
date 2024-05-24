d75b1e4ab29644778a257b59381d924d922005fa
Віталій Чолан <vitalikcolan@gmail.com>
2024-05-24 19:22:44 +0300

    added nested accordions with smooth animations

--- js/hamburgerMenu.js
+++ js/hamburgerMenu.js
@@ -3,10 +3,37 @@ const dropdown = document.querySelector('.dropdown');
 
 toggleButton.addEventListener('click', () => {
   const isActive = dropdown.classList.toggle('active');
-
   if (isActive) {
     dropdown.style.maxHeight = `${dropdown.scrollHeight}px`;
+    setTimeout(() => {
+      dropdown.style.maxHeight = 'initial';
+    }, 1000);
   } else {
-    dropdown.style.maxHeight = '0px';
+    dropdown.style.maxHeight = `${dropdown.scrollHeight}px`;
+    setTimeout(() => {
+      dropdown.style.maxHeight = null;
+    }, 0);
   }
+});
+
+const dropdownItems = document.querySelectorAll('.dropdown-item');
+
+dropdownItems.forEach((dropdownItem) => {
+  dropdownItem.addEventListener('click', () => {
+    const dropdownMenu = dropdownItem.querySelector('.dropdown-menu');
+
+    const isActive = dropdownMenu.classList.toggle('active');
+
+    if (isActive) {
+      dropdownMenu.style.maxHeight = `${dropdownMenu.scrollHeight}px`;
+      setTimeout(() => {
+        dropdownMenu.style.maxHeight = 'initial';
+      }, 1000);
+    } else {
+      dropdownMenu.style.maxHeight = `${dropdownMenu.scrollHeight}px`;
+      setTimeout(() => {
+        dropdownMenu.style.maxHeight = null;
+      }, 0);
+    }
+  });
 });
